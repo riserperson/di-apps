85101921221eba8fcd92ab57758e00fd5beeeb5b
riserperson <richard.iserman@gmail.com>
2019-02-18 10:53:47 -0500

    Filling in the gaps.

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,28 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'rai'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Matthew'},
+            'body': 'I wrote the first book of the new testament'
+        },
+        {
+            'author': {'username': 'Paul'},
+            'body': 'I wrote way more than all yall bitches'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form) 
