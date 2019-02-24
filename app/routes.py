84d7c20bb85240c8bdead2d6c3c32285755b1662
rai <richard.iserman@gmail.com>
2019-02-24 11:34:21 -0500

    Reworked to use bootstrap and included searcher() function.

--- app/routes.py
+++ app/routes.py
@@ -1,71 +1,23 @@
-from flask import render_template, flash, redirect, request, url_for
+import os
+from flask import render_template, flash, redirect, request, url_for, send_file
 from werkzeug.urls import url_parse
-from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
-from app import db
-from app import app
-from app.forms import LoginForm, RegistrationForm
+from werkzeug import secure_filename
+from app import app, db
+from app.forms import AcrolistForm
+from acrolist import searcher
 
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    user = {'username': 'rai'}
-    posts = [
-        {
-            'author': {'username': 'Matthew'},
-            'body': 'I wrote the first book of the new testament'
-        },
-        {
-            'author': {'username': 'Paul'},
-            'body': 'I wrote way more than all yall bitches'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+UPLOAD_PATH = '/home/rai/projects/di_apps/uploads/'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form) 
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
-
-@app.route('/user/<username>')
-@login_required
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
-
+@app.route('/', methods=['GET', 'POST'])
+def acrolist():
+    form = AcrolistForm()
+    if form.doc.data:
+#        filename = secure_filename(form.doc.data.filename)
+#        form.doc.data.save('uploads/' + filename) 
+        use_parens = form.use_parens.data
+        acronyms = searcher(form.doc.data)        
+        f = open('output.txt','w')
+        for i in acronyms:
+            f.write(str(acronyms[1]) + ' ' + str(acronyms[2])+'\n')
+        return send_file(f)
+    return render_template('acrolist.html', form=form)
