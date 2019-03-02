1acbfff5440b7ffd197bb0210b46caa4af3d53ca
rai <richard.iserman@gmail.com>
2019-03-02 12:38:59 -0500

    Trying to get the regex in the searcher to work better

--- app/acrolist.py
+++ app/acrolist.py
@@ -1,18 +1,17 @@
-# coding=utf8
-# the above tag defines encoding for this document and is for Python 2.x compatibility
-
 import re
 
-def searcher(f):
-    regex = ur"(\b(?:[A-Z]){2,}\s)(\(.*?\))"
+acronyms = {
+}
 
-    acronyms = {
+def searcher(f, use_parens=False):
 
-    }
+    if use_parens == True:
+        regex = something_cool
+    else:
+        regex = ur'[A-Z]{2,}'
+
     for line in f:
-        input_str = line
-        matches = re.finditer(regex, input_str, re.MULTILINE)
-        for matchNum, match in enumerate(matches, start=1):
-            if match.group(1) not in acronyms.keys():
-                acronyms[match.group(1)] = match.group(2)
-                return acronyms
+        for m in re.finditer(regex, line):
+            acronyms[m.group(0)] = m.group(0)
+    return acronyms
+
